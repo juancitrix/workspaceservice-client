417ae24464e6ca8b06ad396be401eb63e2103c65
Juan Rivera <juan.rivera@citrix.com>
2019-01-27 14:26:27 -0500

    First positive test case

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,17 @@
-import { Greeter } from '../index';
+import { getPrimaryDesktopConnection } from '../index';
+import { ConnectionResponse } from '../connection-response';
 
-test('My Greeter', () => {
-  expect(Greeter('Carl')).toBe('Hello Carl');
+test('Get default desktop', async () => {
+
+  const username = process.env.TESTUSERNAME || '';
+  const password = process.env.TESTPASSWORD || '';
+  const hostname = process.env.TESTHOSTNAME || '';
+
+  expect(username).not.toBe('');
+  expect(password).not.toBe('');
+  expect(hostname).not.toBe('');
+
+  const response: ConnectionResponse = await getPrimaryDesktopConnection(username, password, hostname);
+
+  expect(response.code).toBe(200);
 });
